c7c601952a83e5971b9397acc9de21474ed15f85
Your Name <you@example.com>
2026-05-07 13:56:52 +0000

    fix: 使用相对路径 ./assets/

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { traeBadgePlugin } from 'vite-plugin-trae-solo-badge';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/penguin-hop/',
+  base: './',
   build: {
     sourcemap: 'hidden',
   },
